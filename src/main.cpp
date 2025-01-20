3e30246c6c286fce4fa667d09e25122762f0ca0a
Akshobya Rao <akshu.theracecarfan@gmail.com>
2025-01-20 17:39:18 -0600

    KU
    KU

--- src/main.cpp
+++ src/main.cpp
@@ -1,246 +1,292 @@
 #include "main.h"
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/abstract_motor.hpp"
+#include "pros/adi.hpp"
 #include "pros/misc.h"
+#include "pros/misc.hpp"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "pros/motor_group.hpp"
+#include "pros/motors.h"
 #include "pros/motors.hpp"
-#include "lemlib/api.hpp"
+#include "pros/rotation.hpp"
 
 
-bool firstPos = true;
-bool secondPos = false;
-bool thirdPos = false;
+pros::Motor babybrown(5);
+
+pros::Rotation babymove(20);
+
+int primemodewall=30;
+
+pros::MotorGroup leftweels({-1,	-2,-3},pros::MotorGearset::blue);
+pros::MotorGroup rightweels({13,12,18},pros::MotorGearset::blue);
+
+pros::Motor convey(14);
+
+pros::adi::Pneumatics clamp1('A',false);
+pros::adi::Pneumatics clamp2('B',false);
+
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor intake(11);
+lemlib::Drivetrain drivetrain(&leftweels,
+								&rightweels,
+								13.75,
+								lemlib::Omniwheel::NEW_275,
+								450,
+								2
 
 
-// Controller
-pros::Controller master(pros::E_CONTROLLER_MASTER); //! SIGMA CONTROLLER 🤖
-lemlib::ExpoDriveCurve driveCurve(3, 10, 1.019);
 
-// Motor Groups
-pros::MotorGroup left_mg({-1, -2, -3}, pros::MotorGearset::blue);    
-pros::MotorGroup right_mg({5, 11, 8}, pros::MotorGearset::blue); 
 
-// Single Motors
-pros::Motor flexwheel(18, pros::MotorGearset::green);
-pros::Motor chain(15, pros::MotorGearset::blue);
-pros::Motor ladyBrown(21, pros::MotorGearset::green);
+	);
+lemlib::OdomSensors sensors(nullptr,nullptr,nullptr,nullptr,nullptr);
+lemlib::ControllerSettings lateral_movement(1000, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              0, // derivative gain (kD)
+                                              0, // anti windup
+                                              1, // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
+                                              30 // maximum acceleration (slew)
+);
 
-// Solenoids
-pros::adi::Pneumatics mogo('A', false);
-pros::adi::Pneumatics doinker('B', false); //? IT DOINKS!!!!
-
-// Drivetrain
-lemlib::Drivetrain drivetrain(
-	&left_mg, // left motor group
-    &right_mg, // right motor group
-    11.25, // 11.25 inch track width
-	lemlib::Omniwheel::NEW_325, // using new 3.25" omnis //! Check wheel size
-    450, // drivetrain rpm is 450
-    2 // horizontal drift is 2 (for now)
+// angular PID controller
+lemlib::ControllerSettings angular_movement(1000, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              1, // derivative gain (kD)
+                                              0, // anti windup
+                                                 0, // small error range, in degrees
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in degrees
+                                              0, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_movement, // lateral PID settings
+                        angular_movement, // angular PID settings
+                        sensors // odometry sensors
 );
 
 
-// sensors
-pros::Imu imu(10); // degrees/turning
-pros::Distance distance_sensor(6);  // Assuming it's connected to port 1
-
-
-
-// pros::Rotation xOdom(6); // x pos place on left or right sides at base of dt
-pros::Rotation yOdom(-7); // y pos place on front or back of dt
-// pros::Rotation ladyOdom(9); // y pos place on front or back of dt
-
-//! Need to adjust offsets -> Distance from Center of wheel to center of tracking wheel(left -> neg; right -> pos)
-
-// lemlib::TrackingWheel horizontal_tracking_wheel(&xOdom, lemlib::Omniwheel::NEW_275, -5.75);
-lemlib::TrackingWheel vertical_tracking_wheel(&yOdom, lemlib::Omniwheel::NEW_2, -2.5); //! offset
-// lemlib::TrackingWheel lady_tracking_wheel(&yOdom, lemlib::Omniwheel::NEW_275, -2.5);
-
-lemlib::OdomSensors sensors(
-    &vertical_tracking_wheel, // vertical tracking wheel 1, set to null
-    nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
-    nullptr, // horizontal tracking wheel 1
-    nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
-    &imu // inertial sensor
-);
-
-
-// PID(After you tune, everything should work as expect, if not look at the printscreens to see if the robot thinks its getting to such positions, if so, then its likely a PID tuning issue, will take sometime but you have time)
-// Do default, set 0, slowly adjust each factor, 
-// at the end, should have smooth motions
-
-// TODO: Tune PID again with new sensors
-
-lemlib::ControllerSettings lateral_controller(
-	10, // proportional gain (kP)
-    0, // integral gain (kI)
-    3, // derivative gain (kD)
-    3, // anti windup
-    1, // small error range, in inches
-	100, // small error range timeout, in milliseconds
-    3, // large error range, in inches
-    500, // large error range timeout, in milliseconds
-    20 // maximum acceleration (slew)
-);
-
-
-lemlib::ControllerSettings angular_controller(
-	2, // proportional gain (kP)
-	0, // integral gain (kI)
-    10, // derivative gain (kD)
-	3, // anti windup
-    1, // small error range, in degrees
-    100, // small error range timeout, in milliseconds
-    3, // large error range, in degrees
-    500, // large error range timeout, in milliseconds
-    0 // maximum acceleration (slew)
-);
-
-// create the chassis
-lemlib::Chassis chassis(
-    drivetrain, // drivetrain settings
-    lateral_controller, // lateral PID settings
-    angular_controller, // angular PID settings
-    sensors, // odometry sensors
-    &driveCurve,
-    &driveCurve
-);
-
-//? Random code ->
-
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
+void initialize(){
+    pros::lcd::initialize();
+	leftweels.tare_position();
+	pros::delay(500);
+	rightweels.tare_position();
+	pros::delay(500);
+	chassis.calibrate();
+	pros::delay(500);
+	while (true){
+		pros::lcd::print(0,"X:%f",chassis.getPose().x);
+		pros::lcd::print(0,"Y:%f",chassis.getPose().y);
+		pros::lcd::print(0,"THETA:%f",chassis.getPose().theta);
+		pros::delay(20);
+		
 	}
 }
+void disabled() {}
 
-void initialize() {
-	pros::lcd::initialize();
-    // calibrate sensors
-	imu.set_heading(0); // Set IMU heading to 0 for reference
-    pros::delay(100); // Optional delay for IMU calibration
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
 
-    // xOdom.reset_position(); // Reset X odom sensor
-    yOdom.reset_position(); // Reset Y odom sensor
-	pros::delay(100); // Optional delay for IMU calibration
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */ 
 
-    chassis.calibrate(); // Calibrate the chassis to reset odometry
-	pros::delay(500);
+/*void allianceAndSingularRingright(){
+	chassis.moveToPoint(0,12,2000);
+	chassis.turnToHeading(90,2000);
+	chassis.moveToPoint(5, 12, 2000);
+	babybrown.move(-100);
+	pros::delay(200);
+	babybrown.brake();
+	babybrown.move(100);
+	pros::delay(200);
+	babybrown.brake();
+	chassis.moveToPoint(-48, -12, 0);
+	chassis.turnToHeading(-90, 5000);
+	clamp1.extend();
+	clamp2.extend();
+	chassis.turnToHeading(-180, 3000);
+	intake.move(100);
+	convey.move(100);
+	
+}
+void allianceAndSingularRingrightey(){
+	chassis.moveToPoint(0,12,2000);
+	chassis.turnToHeading(-90,2000);
+	chassis.moveToPoint(5, 12, 2000);
+	babybrown.move(-100);  
+	pros::delay(200);
+	babybrown.brake();
+	babybrown.move(100);
+	pros::delay(200);
+	babybrown.brake();
+	chassis.moveToPoint(-48, -12, 0);
+	chassis.turnToHeading(-90, 2000);
+	clamp1.extend();
+	clamp2.extend();
+	chassis.turnToHeading(0, 2000);
+	chassis.moveToPoint(-48, 12, 2000);
+	intake.move(100);
+	convey.move(100);
+	
+}
+void skilz(){
+	chassis.setPose(0,0,0);
+	intake.move(100);
+	convey.move(100);
+	chassis.moveToPoint(0, 12, 2000);//moves up
+	chassis.turnToHeading(90,5000 );//turns toward mogo1
+	chassis.moveToPoint(-24,12, 2000);//moves backward to mogo1
+	clamp1.extend();//CLAMPS
+	clamp2.extend();
+	chassis.turnToHeading(0, 4000);//turns to a ring
+	chassis.moveToPoint(-24, 36,4000);//goes around to get four rings
+	chassis.turnToHeading(-90, 2000);
+	chassis.moveToPoint(-48, 36, 2000);
+	chassis.turnToHeading(180, 1000);
+	chassis.moveToPoint(-48, 0, 2000);//done getting four rings
+	chassis.moveToPoint(-48, 12, 1200);//goes back
+	chassis.turnToHeading(-90, 2000);//turns to fifth ring
+	chassis.moveToPoint(-72, 12, 2000);//intakes fifth ringy
+	chassis.moveToPoint(-48, 12, 1000);//goes back
+	chassis.turnToHeading(45, 2000);//turns to cornerr
+	chassis.moveToPoint(-72, 0, 2000);//goes into corner
+	clamp1.retract();
+	clamp2.retract();
+	chassis.moveToPoint(0, 0, 2000);//goes back to beginning position for ease
 
-	pros::lcd::print(1, "Calibration Complete 🍎 Sigma");
+	chassis.setPose(0,0,0);
+	intake.move(100);
+	convey.move(100);
+	chassis.moveToPoint(0, 12, 2000);//moves ujp
+	chassis.turnToHeading(-90,5000 );//turns to mogo
+	chassis.moveToPoint(24,12, 2000);//goes back into mogo
+	clamp1.extend();//clamps
+	clamp2.extend();
+	chassis.turnToHeading(0, 4000);//turns to the four rings 
+	chassis.moveToPoint(24, 36,4000);//goes around in a circle for four rings
+	chassis.turnToHeading(90, 60000);
+	chassis.moveToPoint(48, 36, 2000);
+	chassis.turnToHeading(180, 1000);
+	chassis.moveToPoint(48, 0, 2000);//done going in circle for four rings
+	chassis.moveToPoint(48, 12, 1200);//goes back
+	chassis.turnToHeading(-90, 2000);//turns to fifth ring
+	chassis.moveToPoint(72, 12, 2000);//intakes fifth ring
+	chassis.moveToPoint(48, 12, 1000);//goes back
+	chassis.turnToHeading(-45, 2000);//turns to corner
+	chassis.moveToPoint(72, 0, 2000);//goes into corner
+
+	chassis.moveToPoint(60, 12, 2000);//goes to line up with ring for lady brown
+	chassis.turnToHeading(0, 2000);//turns to ring for lady brown
+	babybrown.move_relative(30, 20);//moves lady brown up to be primed
+	chassis.moveToPoint(60,60,2000 );//intakes ring for lady brown
+	chassis.turnToHeading(90, 2000);//turns for wall stake
+	babybrown.move(100);//moves lady brown for quarter second
+	pros::delay(250);
+	babybrown.brake();//stops lady brown
+	chassis.setPose(0,0,0);
+	chassis.moveToPoint(0, -12, 3000);//moves back a little
+	chassis.turnToHeading(22.5, 2000);//turns to blue goal
+	chassis.moveToPoint(60, -36, 3000);//moves it into the goal
+	clamp1.extend();//clamps
+	clamp2.extend();
+	chassis.turnToHeading(180, 2000);//turns to the corner
+	chassis.moveToPoint(60, 0, 2000);//puts into corner
+	clamp1.retract();//unclamps
+	clamp2.retract();
+	chassis.moveToPoint(60,-156,3000);//puts other blue goal into corner
 
 
-	pros::Task screen_task([&]() {
-        while (true) {
-            // print robot location to the brain screen
-            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
-            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
-            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-            pros::delay(20);
-        }
-    });
+
+
+}
+*/
+void autonomous(){
+	chassis.setPose(0,0,0);
+	chassis.turnToHeading(90, 5000);
+	chassis.waitUntilDone();
 }
 
-void disabled() {} // NOT NEEDED!!!
 
-void competition_initialize() {} // NOT NEEDED!
 
-void autonomous() {
-    
-	imu.set_heading(0);
-	chassis.setPose(0,0,0);
-
-    chain.move(400);
-    pros::delay(2000);
-    chain.move(0);
-
-    // EXAMPLE
-
-    // chassis.moveToPose(
-    //     48,
-    //     -24,
-    //     90,
-    //     2000,
-    //     {.minSpeed=72, .earlyExitRange=8}
-    // );
-
-} 
-
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
 void opcontrol() {
 	while (true) {
+		
+		leftweels.move(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+		rightweels.move(master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
 
-        // movement
-		int leftY = master.get_analog(ANALOG_LEFT_Y);
-		int rightY = master.get_analog(ANALOG_RIGHT_Y);
-		left_mg.move(leftY);
-		right_mg.move(rightY);
+		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
+			intake.move(-1000);
+			convey.move(1000);
+	
+		}
+		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+			intake.move(1000);
+			convey.move(-1000);
 
-        int sensor_value = distance_sensor.get_distance();
-
-
-		pros::delay(20); // Run for 20 ms then update
-
-        //! Mogo Clamp
-        if(master.get_digital(DIGITAL_B)){
-            mogo.extend();
-        } else if(master.get_digital(DIGITAL_DOWN)){
-            mogo.retract();
-        }
-
-        if(master.get_digital(DIGITAL_RIGHT)){
-            if(sensor_value < 10.0){
-                pros::delay(10);
-                chain.move(-400);
-                flexwheel.move(-400);
-                pros::delay(800);  // Delay for 500ms (duration of the outtake)
-                chain.move(0);
-                flexwheel.move(0);
-            }
-        }
-
-        //! Doinker;
-        if(master.get_digital(DIGITAL_L1)){
-            doinker.set_value(true);
-        } else {
-            doinker.set_value(false);
-        }
+		}
+		else{
+			intake.brake();
+			convey.brake();
+		}
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && ((clamp2.is_extended()==false)&&(clamp1.is_extended()==false))){
+			clamp1.extend();
+			clamp2.extend();
+			pros::delay(250);
+		}
+		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && ((clamp2.is_extended()==true)&&(clamp1.is_extended()==true))){
+			clamp1.retract();
+			clamp2.retract();
+			pros::delay(250);
+		}
 
 
-        //! Intake
-        if(master.get_digital(DIGITAL_R1)){
-            chain.move(400);
-            flexwheel.move(400);
-        } else if(master.get_digital(DIGITAL_R2)){
-            chain.move(-400);
-            flexwheel.move(-400);
-        } else{
-            chain.move(0);
-            flexwheel.move(0);
-        }
+			convey.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		}
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
+			babybrown.move(50);
+		}
+		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)){
+			babybrown.move(-50);
+		}
+		else{
+			babybrown.brake();
+		}
 
-        //! Lady Brown
-        if(master.get_digital(DIGITAL_L2)) {
-            if(firstPos == true && secondPos == false && thirdPos == false){
-                    ladyBrown.move_absolute(390, 200);
-                    firstPos = false;
-                    secondPos = true;
-                    thirdPos = false;
-            } 
-            else if(firstPos == false && secondPos == true && thirdPos == false){
-                    ladyBrown.move_absolute(1600, 200);
-                    firstPos = false;
-                    secondPos = false;
-                    thirdPos = true;
-            }
-            else if(firstPos == false && secondPos == false && thirdPos == true){
-                    ladyBrown.move_absolute(0, 250);
-                    firstPos = true;
-                    secondPos = false;
-                    thirdPos = false;
-            }   
-        }
-        
 
-	}
+
+
+	pros::delay(20);
 }
